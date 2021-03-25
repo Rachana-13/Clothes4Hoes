9ff8c3e53d7f84a5f49868ff5e678f41beb8c2bf
SamHulleman <109807@st-maartenscollege.nl>
2021-03-25 17:08:13 +0100

    Toegevoegd: Error folder toegevoegd      NotFound.cshtml Toegevoegd InlogModel.cs Toegevoegd ErrorController.cs Toegevoegd
    Veranderingen:
     HomeController:
     Inloggen functies vernadert en een model aan toegevoegt.
    Inloggen.cshtml:
    oude inlog form verwijdert en de juist functionerende form toegevoegt.

---
+++ Clothes4Hoes/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clothes4Hoes.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/Error/{StatusCode}")]
+        public IActionResult Index(int StatusCode)
+        {
+            string Message = string.Empty;
+
+            switch (StatusCode)
+            {
+                case 404:
+                    Message = "helaas pagina niet gevonden";
+                    break;
+            }
+            return View("NotFound");
+        }
+    }
+}
---
+++ Clothes4Hoes/Models/InlogModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Clothes4Hoes.Models
+// namespace SchoolTemplate.Models
+{
+    public class InlogModel
+    {
+        //error messages als de invoer fout is 
+        [Required(ErrorMessage = "Voornaam is verplicht!")]
+        public string Voornaam { get; set; }
+
+        [Required(ErrorMessage = "Achternaam is verplicht!")]
+        public string Achternaam { get; set; }
+
+        [Required(ErrorMessage ="E-mail is verplicht!")]
+        [EmailAddress(ErrorMessage = "Geen geldig e-mailadres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Geboortedatum is verplicht!")]
+        public DateTime? Geboortedatum { get; set; }
+
+        [Required(ErrorMessage = "Wachtwoord is verplicht!")]
+        public string Wachtwoord { get; set; }
+
+       
+
+    }
+}
